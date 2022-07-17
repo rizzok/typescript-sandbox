d01aff6410d70219c503b9da1a542cd1e4dc81d9
rizzok <kevinrizzo13100@gmail.com>
2022-07-17 16:29:16 +0200

    Grafikart 11: Type Challenge easy

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,39 @@
 "use strict";
-const compteur = document.getElementById('compteur');
-let i = 0;
-const increment = (e) => {
-    e.preventDefault();
-    i++;
-    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
-    if (span)
-        span.innerHTML = i.toString();
-};
-compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
+/*
+  4 - Pick
+  -------
+  by Anthony Fu (@antfu) #easy #union #built-in
+
+  ### Question
+
+  Implement the built-in `Pick<T, K>` generic without using it.
+
+  Constructs a type by picking the set of properties `K` from `T`
+
+  For example:
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+  const todo: TodoPreview = {
+      title: 'Clean room',
+      completed: false,
+  }
+  ```
+
+  > View on GitHub: https://tsch.js.org/4
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4/answer
+  > View solutions: https://tsch.js.org/4/solutions
+  > More Challenges: https://tsch.js.org
+*/
 //# sourceMappingURL=app.js.map
