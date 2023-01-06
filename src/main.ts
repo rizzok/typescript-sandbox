b2713b20afb132ea61bba035087b129a69f5681f
rizzok <suncode13@gmail.com>
2023-01-06 21:35:26 +0100

    practice classes

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,37 @@
-type ID = number;
-
 interface UserInterface {
-  id: ID;
-  pseudo: string;
+  getFullname(): string
 }
 
-const user1: UserInterface = {
-  id: 1,
-  pseudo: "Johnny92"
+class User implements UserInterface {
+  private firstName: string
+  private lastName: string
+  readonly unchangableName: string
+  static readonly maxAge = 50
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+  
+  getFullname(): string {
+    return this.firstName + ' ' + this.lastName
+  }
 }
 
-const doSomething = (): void => {
-  console.log("do something");
+class Admin extends User {
+  private editor: string
+
+  setEditor(editor: string) {
+    this.editor = editor
+  }
+
+  getEditor(): string {
+    return this.editor
+  }
 }
+
+const user1 = new User('John', 'Doe')
+
+const admin1 = new Admin('Titi', 'Toto')
+
+admin1
